3a3131d6c47b28fcd6862715d6c8c1c8aac3906a
PerchunPak <git@perchun.it>
2024-06-02 00:07:55 +0200

    Support multiple Seffro ids and put it in config

--- src/__main__.py
+++ src/__main__.py
@@ -4,12 +4,11 @@ import src.config
 
 config = src.config.Config.parse()
 bot = hikari.GatewayBot(intents=hikari.Intents.MESSAGE_CONTENT | hikari.Intents.GUILD_MESSAGES, token=config.token)
-SEFFRO_ID = 379353300887273472  # TODO that's my ID, what's seffro id?
 
 
 @bot.listen()
 async def log_seffro(event: hikari.GuildMessageCreateEvent) -> None:
-    if event.author_id != SEFFRO_ID:
+    if event.author_id not in config.seffro_ids:
         return
 
     print("Seffro wrote:", event.content)
--- src/config.py
+++ src/config.py
@@ -9,12 +9,17 @@ class ImproperConfigurationError(Exception): ...
 @dataclasses.dataclass
 class Config:
     token: str
+    seffro_ids: list[int]
 
     @classmethod
     def parse(cls) -> t.Self:
         if "BOT_TOKEN" not in os.environ:
             raise ImproperConfigurationError("Please provide bot token as `BOT_TOKEN` environment variable")
+        if "SEFFRO_IDS" not in os.environ:
+            raise ImproperConfigurationError("Please provide IDs separated by `,` to log in `SEFFRO_IDS`")
 
+        seffro_ids = list(map(int, os.environ["SEFFRO_IDS"].split(",")))
         return cls(
             token=os.environ["BOT_TOKEN"],
+            seffro_ids=seffro_ids,
         )
